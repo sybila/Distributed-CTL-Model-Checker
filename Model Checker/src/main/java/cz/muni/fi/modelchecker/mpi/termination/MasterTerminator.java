0a46819564b7b996a6e20d6953b3481817dbc72f
daemontus <daemontus@gmail.com>
2015-01-06 17:53:48 +0100

    Termination refactoring

--- Model Checker/src/main/java/cz/muni/fi/modelchecker/mpi/termination/MasterTerminator.java
+++ Model Checker/src/main/java/cz/muni/fi/modelchecker/mpi/termination/MasterTerminator.java
@@ -4,45 +4,33 @@ package cz.muni.fi.modelchecker.mpi.termination;
  * Terminator running on a machine that has rank 0
  * Uses Safra's algorithm(http://fmt.cs.utwente.nl/courses/cdp/slides/cdp-8-mpi-2-4up.pdf) for termination detection.
  */
-public class MasterTerminator implements Terminator {
+public class MasterTerminator extends Terminator {
 
-    private boolean flag = false;
-    private int count = 0;
-
-    private boolean working = false;
     private boolean waitingForToken = false;
-    private boolean finalized = false;
 
-    private final int tokenSource;
-    private final int tokenDestination;
-    private final TokenMessenger messenger;
-
-    public MasterTerminator(TokenMessenger messenger) {
+    public MasterTerminator(TokenMessenger m) {
+        super(m, (m.getMyId() + 1) % m.getProcessCount(), m.getProcessCount() - 1);
         if (messenger.getMyId() != 0) {
             throw new IllegalStateException("Cannot launch master terminator on slave machine");
         }
-        this.messenger = messenger;
-        int size = messenger.getProcessCount();
-        tokenSource = (messenger.getMyId() + 1) % size;
-        tokenDestination = size - 1;
+      //  System.out.println("Const");
     }
 
-    private synchronized void initProbe() {
+    private void initProbe() {
+      //  System.out.println("Init probe");
         //status = White
-      //  System.out.println("Init Probe");
-        flag = false;
+        flag = 0;
         waitingForToken = true;
         messenger.sendTokenAsync(tokenDestination, new Token(0,0));
     }
 
-    private synchronized void initTermination() {
+    private void initTermination() {
         messenger.sendTokenAsync(tokenDestination, new Token(2,0));
     }
 
     @Override
     public synchronized void setWorking(boolean working) {
-        if (finalized) throw new IllegalStateException("Called setWorking on finalized master terminator");
-        this.working = working;
+        super.setWorking(working);
         if (!working && !waitingForToken) {
             //if node is idle and no token is already in the system
             //Note: It is ok to send a probe even when main task has not finished yet, because
@@ -53,57 +41,30 @@ public class MasterTerminator implements Terminator {
     }
 
     @Override
-    public synchronized void messageSent() {
-        if (finalized) throw new IllegalStateException("Called messageSent on finalized master terminator");
-        count++;
-    }
-
-    @Override
-    public synchronized void messageReceived() {
-        if (finalized) throw new IllegalStateException("Called messageReceived on finalized master terminator");
-        count--;
-        //status = Black
-        flag = true;
-    }
-
-    @Override
-    public void waitForTermination() {
-        if (finalized) throw new IllegalStateException("Called waitForTermination on finalized master terminator");
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (MasterTerminator.this) {
-                    if (!working && !waitingForToken) initProbe();
+    public void terminationLoop() {
+        synchronized (this) {
+         //   System.out.println("Term loop: "+working+" "+waitingForToken);
+            if (!working && !waitingForToken) initProbe();
+        }
+        Token token = messenger.waitForToken(tokenSource);
+        while (token.flag < 2) {
+            synchronized (this) {
+                if (!waitingForToken) {
+                    throw new IllegalStateException("Master received a token he was not waiting for!");
                 }
-                Token token = messenger.waitForToken(tokenSource);
-                while (token.flag < 2) {
-                    synchronized (MasterTerminator.this) {
-                        if (!waitingForToken) {
-                            throw new IllegalStateException("Master received a token he was not waiting for!");
-                        }
-                        //   System.out.println("Probe returned to master: "+flag+" "+token.flag+" "+count+" "+token.count);
-                        waitingForToken = false;
-                        if (!flag && token.flag == 0 && token.count + count == 0) {
-                            //if termination criteria are met, finish this whole thing
-                            //Slaves should pass this and return it to us (that will terminate this while loop)
-                            initTermination();
-                        } else {
-                            if (!working) {
-                                //if node is idle, just go for another round
-                                initProbe();
-                            } //if we are working, just wait - setWorking will init the probe
-                        }
-                    }
-                    token = messenger.waitForToken(tokenSource);
+                waitingForToken = false;
+                if (flag == 0 && token.flag == 0 && token.count + count == 0) {
+                    //if termination criteria are met, finish this whole thing
+                    //Slaves should pass this and return it to us (that will terminate this while loop)
+                    initTermination();
+                } else {
+                    if (!working) {
+                        //if node is idle, just go for another round
+                        initProbe();
+                    } //if we are working, just wait - setWorking will init the probe
                 }
             }
-        });
-        t.start();
-        try {
-            t.join();
-            finalized = true;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            token = messenger.waitForToken(tokenSource);
         }
     }
 }
--- Model Checker/src/main/java/cz/muni/fi/modelchecker/mpi/termination/SlaveTerminator.java
+++ Model Checker/src/main/java/cz/muni/fi/modelchecker/mpi/termination/SlaveTerminator.java
@@ -4,29 +4,18 @@ package cz.muni.fi.modelchecker.mpi.termination;
  * Terminator running on a machine that has rank different than 0.
  * Uses Safra's algorithm(http://fmt.cs.utwente.nl/courses/cdp/slides/cdp-8-mpi-2-4up.pdf) for termination detection.
  */
-public class SlaveTerminator implements Terminator {
+public class SlaveTerminator extends Terminator {
 
-    private int flag = 0;
-    private int count = 0;
-
-    private boolean working = false;
     private Token pendingToken = null;
 
-    private final int tokenSource;
-    private final int tokenDestination;
-    private final TokenMessenger messenger;
-
-    public SlaveTerminator(TokenMessenger messenger) {
-        this.messenger = messenger;
-        int rank = messenger.getMyId();
-        tokenSource = (rank + 1) % messenger.getProcessCount();
-        tokenDestination = rank - 1;
+    public SlaveTerminator(TokenMessenger m) {
+        super(m, (m.getMyId() + 1) % m.getProcessCount(), m.getMyId() - 1);
         if (tokenDestination < 0) {
             throw new IllegalStateException("Cannot launch slave terminator on master machine");
         }
     }
 
-    private synchronized void processToken(Token token) {
+    private void processToken(Token token) {
         //non blocking send (so that we do not end up stuck in synchronized section)
         messenger.sendTokenAsync(tokenDestination, new Token(Math.min(token.flag + flag, 1), token.count + count));
         //status = White
@@ -34,49 +23,25 @@ public class SlaveTerminator implements Terminator {
     }
 
     @Override
-    public synchronized void messageSent() {
-        count++;
-    }
-
-    @Override
-    public synchronized void messageReceived() {
-        count--;
-        //status = Black
-        flag = 1;
-    }
-
-    @Override
-    public void waitForTermination() {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Token token = messenger.waitForToken(tokenSource);
-                while (token.flag < 2) {
-                    synchronized (SlaveTerminator.this) {
-                        //   System.out.println(messenger.getMyId()+" probe received: "+flag+" "+token.flag+" "+count+" "+token.count);
-                        if (working) {  //if node is active, save token for later
-                            pendingToken = token;
-                        } else { //else pass token to next node right away
-                            processToken(token);
-                        }
-                    }
-                    token = messenger.waitForToken(tokenSource);
+    public void terminationLoop() {
+        Token token = messenger.waitForToken(tokenSource);
+        while (token.flag < 2) {
+            synchronized (SlaveTerminator.this) {
+                if (working) {  //if node is active, save token for later
+                    pendingToken = token;
+                } else { //else pass token to next node right away
+                    processToken(token);
                 }
-                //termination message has been received - we pass this to next node and finish ourselves
-                messenger.sendTokenAsync(tokenDestination, token);
             }
-        });
-        t.start();
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            token = messenger.waitForToken(tokenSource);
         }
+        //termination message has been received - we pass this to next node and finish ourselves
+        messenger.sendTokenAsync(tokenDestination, token);
     }
 
     @Override
     public synchronized void setWorking(boolean working) {
-        this.working = working;
+        super.setWorking(working);
         if (!working && pendingToken != null) {
             //node is idle and has unprocessed tokens
             processToken(pendingToken);

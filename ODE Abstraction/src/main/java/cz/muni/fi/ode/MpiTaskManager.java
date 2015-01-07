41003ccef623311b2a8438a3013b42cde9249d39
daemontus <daemontus@gmail.com>
2015-01-07 13:09:44 +0100

    Parameter count fix

--- ODE Abstraction/src/main/java/cz/muni/fi/ode/MpiTaskManager.java
+++ ODE Abstraction/src/main/java/cz/muni/fi/ode/MpiTaskManager.java
@@ -27,11 +27,13 @@ public class MpiTaskManager extends TaskManager<CoordinateNode,TreeColorSet> {
     private final Comm COMM;
     private final int dimensions;
     private final NodeFactory factory;
+    private final OdeModel model;
 
     public MpiTaskManager(
             Comm comm,
             int dimensions,
             NodeFactory factory,
+            OdeModel model,
             Formula activeFormula,
             Terminator terminator,
             FormulaVerificator<CoordinateNode, TreeColorSet> verificator) {
@@ -39,6 +41,7 @@ public class MpiTaskManager extends TaskManager<CoordinateNode,TreeColorSet> {
         this.COMM = comm;
         this.dimensions = dimensions;
         this.factory = factory;
+        this.model = model;
     }
 
     /**
@@ -49,14 +52,14 @@ public class MpiTaskManager extends TaskManager<CoordinateNode,TreeColorSet> {
 
     @Override
     protected void sendTask(int destinationNode, CoordinateNode internal, CoordinateNode external, TreeColorSet colors) {
-        @NotNull int[] buffer = new int[3*dimensions + 3];
+        @NotNull int[] buffer = new int[2*dimensions + model.parameterCount() + 3];
         buffer[0] = CREATE;
         buffer[buffer.length - 2] = COMM.Rank();
         buffer[buffer.length - 1] = 0;
         System.arraycopy(internal.coordinates, 0, buffer, 1, dimensions);
         System.arraycopy(external.coordinates, 0, buffer, dimensions + 1, dimensions);
         @NotNull List<Double> data = new ArrayList<>(2*dimensions);
-        for (int i=0; i<dimensions; i++) {
+        for (int i=0; i<model.parameterCount(); i++) {
             Range<Double>[] ranges = colors.asArrayForParam(i);
             buffer[2*dimensions + i + 1] = 2 * ranges.length;
             for (@NotNull Range<Double> range : ranges) {
@@ -72,7 +75,7 @@ public class MpiTaskManager extends TaskManager<CoordinateNode,TreeColorSet> {
 
     @Override
     protected boolean tryReceivingTask(TaskStarter<CoordinateNode, TreeColorSet> taskStarter) {
-        @NotNull int[] buffer = new int[3*dimensions + 3];
+        @NotNull int[] buffer = new int[2*dimensions + model.parameterCount() + 3];
         //no need to synchronize - this method is only called from one thread
         COMM.Recv(buffer, 0, buffer.length, MPI.INT, MPI.ANY_SOURCE, TAG);
         if (buffer[0] == CREATE) {
@@ -80,7 +83,7 @@ public class MpiTaskManager extends TaskManager<CoordinateNode,TreeColorSet> {
             //int parentId = buffer[buffer.length - 1];
             CoordinateNode source = factory.getNode(Arrays.copyOfRange(buffer, 1, dimensions + 1));
             CoordinateNode dest = factory.getNode(Arrays.copyOfRange(buffer, dimensions + 1, 2 * dimensions + 1));
-            int[] lengths = Arrays.copyOfRange(buffer, 2*dimensions + 1, 3*dimensions + 1);
+            int[] lengths = Arrays.copyOfRange(buffer, 2*dimensions + 1, 2*dimensions + model.parameterCount() + 1);
             double[] colors = new double[sum(lengths)];
             COMM.Recv(colors, 0, colors.length, MPI.DOUBLE, sender, TAG);
             TreeColorSet colorSet = TreeColorSet.createFromBuffer(lengths, colors);
@@ -121,16 +124,18 @@ public class MpiTaskManager extends TaskManager<CoordinateNode,TreeColorSet> {
 
         private final int dimensions;
         private final NodeFactory factory;
+        private final OdeModel model;
 
-        public MpiTaskManagerFactory(int dimensions, NodeFactory factory) {
+        public MpiTaskManagerFactory(int dimensions, NodeFactory factory, OdeModel model) {
             this.dimensions = dimensions;
             this.factory = factory;
+            this.model = model;
         }
 
 
         @Override
         public TaskManager<CoordinateNode, TreeColorSet> createTaskManager(Formula formula, Terminator terminator, FormulaVerificator<CoordinateNode, TreeColorSet> verificator, Comm comm) {
-            return new MpiTaskManager(comm, dimensions, factory, formula, terminator, verificator);
+            return new MpiTaskManager(comm, dimensions, factory, model, formula, terminator, verificator);
         }
     }
 }

a23b60c3fc8ca1cdb16ac7b1dcd7da2bf52585cc
daemontus <daemontus@gmail.com>
2015-09-29 21:11:27 +0200

    First try at Exist Until verification. Requires more work / testing.

--- Model Checker/src/main/java/cz/muni/fi/modelchecker/Messenger.kt
+++ Model Checker/src/main/java/cz/muni/fi/modelchecker/Messenger.kt
@@ -2,15 +2,24 @@ package cz.muni.fi.modelchecker
 
 public interface Messenger<N: Node, C: Colors<C>> {
 
-    fun sendTask(task: Task<N, C>)
+    fun sendTask(task: Job<N, C>)
+
+    fun close()
 
     public interface Factory<N: Node, C: Colors<C>> {
-        fun createNew( onTask: (Task<N, C>) -> Unit ): Messenger<N, C>
+        fun createNew( onTask: (Job<N, C>) -> Unit ): Messenger<N, C>
     }
 }
 
-data class Task<N: Node, C: Colors<C>>(
-        val initNode: N,
-        val targetNode: N,
-        val colors: C
-)
+sealed class Job<N: Node, C: Colors<C>> {
+    data class AUTask<N: Node, C: Colors<C>>(
+            val initNode: N,
+            val targetNode: N,
+            val colors: C
+    ) : Job<N, C>()
+    data class EUTask<N: Node, C: Colors<C>>(
+            val node: N,
+            val colors: C
+    ) : Job<N, C>()
+    class Poison<N: Node, C: Colors<C>>() : Job<N, C>()
+}
--- Model Checker/src/main/java/cz/muni/fi/modelchecker/ModelChecker.kt
+++ Model Checker/src/main/java/cz/muni/fi/modelchecker/ModelChecker.kt
@@ -6,6 +6,8 @@ import cz.muni.fi.ctl.Formula
 import cz.muni.fi.ctl.Op
 import mpjbuf.IllegalArgumentException
 import java.util.*
+import java.util.concurrent.LinkedBlockingQueue
+import kotlin.concurrent.thread
 
 public class ModelChecker<N: Node, C: Colors<C>>(
         partition: Partition<N, C>,
@@ -27,6 +29,7 @@ public class ModelChecker<N: Node, C: Colors<C>>(
                     Op.NEGATION -> checkNegation(f)
                     Op.AND -> checkAnd(f)
                     Op.OR -> checkOr(f)
+                    Op.EXISTS_UNTIL -> checkExistUntil(f)
                     else -> throw IllegalArgumentException("Unsupported operator: ${f.operator}")
                 }
             }
@@ -50,6 +53,70 @@ public class ModelChecker<N: Node, C: Colors<C>>(
         val r0 = verify(f[0])
         val r1 = verify(f[1])
         (r0.keySet() + r1.keySet()).toMap({ it }, { r0.getOrEmpty(it) + r1.getOrEmpty(it) })
+    }
+
+    private fun checkExistUntil(f: Formula): Map<N, C> {
+        val r0 = verify(f[0])
+        val taskQueue = LinkedBlockingQueue<Job<N, C>>()
+        val terminator = terminators.createNew()
+
+
+        val messenger = messengers.createNew {
+            synchronized(taskQueue) {   //Can we do better?
+                terminator.messageReceived()
+                taskQueue.add(it)
+            }
+        }
+
+        val results = HashMap<N, C>()   //No need for sync, updated only from following thread
+
+        //push given colors from given node into it's predecessors
+        val pushBack: (N, C) -> Unit = { node, colors ->
+            for ((predecessor, edgeColors) in node.predecessors()) {
+                val job = Job.EUTask(predecessor, colors intersect edgeColors)
+                if (node.ownerId() == myId) {
+                    taskQueue.add(job)
+                } else {
+                    messenger.sendTask(job)
+                    terminator.messageSent()
+                }
+            }
+        }
+
+        //prepare queue
+        for ((node, colors) in verify(f[1])) {
+            results[node] = colors
+            pushBack(node, colors)
+        }
+
+        val worker = thread {
+            var task = taskQueue.take()
+            while (task is Job.EUTask<N, C>) {
+                val (node, pushed) = task
+
+                //intersect and save as valid
+                val valid = pushed intersect r0.getOrEmpty(node)
+                results[node] = results.getOrEmpty(node) + valid
+
+                pushBack(node, valid)
+
+                synchronized(taskQueue) {   //sync receive/done signals with the queue take
+                    if (taskQueue.isEmpty()) terminator.setDone()
+                    task = taskQueue.take()
+                }
+            }
+            if (task !is Job.Poison) throw IllegalStateException("Somehow a different task got here! $task")
+            //just finish - we received poison, everything is fine :)
+        }
+
+        terminator.waitForTermination()
+
+        return globalBarrier {
+            taskQueue.add(Job.Poison())
+            worker.join()
+            messenger.close()
+            results
+        }
     }
 
     private fun <T> globalBarrier(f: () -> T): T {

2e02f25b587edd334931a7c9b9b09f958845aaaa
Samuel Pastva <daemontus@gmail.com>
2015-10-16 21:20:00 +0200

    Prototype of new AU implementation

--- Model Checker/src/main/java/cz/muni/fi/modelchecker/ModelChecker.kt
+++ Model Checker/src/main/java/cz/muni/fi/modelchecker/ModelChecker.kt
@@ -51,18 +51,15 @@ public class ModelChecker<N: Node, C: Colors<C>>(
 
         val jobQueue = SingleThreadJobQueue(
                 messengers, terminators, partitionFunction,
-                Job.EX(r.keySet().first(), r.values().first()).javaClass  //TODO: There must be a better way to do this
+                Job.EU::class.java as Class<Job.EX<N, C>>  //TODO: There must be a better way to do this
         )
 
         for ((node, colors) in r) {
-            for ((predecessor, edgeColors) in node.predecessors()) {
-                val intersection = colors intersect edgeColors
-                if (intersection.isNotEmpty()) jobQueue.post(Job.EX(predecessor, intersection))
-            }
+            pushBack(node, colors, jobQueue) { s, t, c -> Job.EX(t, c) }
         }
 
         jobQueue.start { val (node, colors) = it
-            result.addOrUnion(node, colors)
+            synchronized(result) { result.addOrUnion(node, colors) }
         }
 
         jobQueue.finish()
@@ -78,15 +75,10 @@ public class ModelChecker<N: Node, C: Colors<C>>(
 
         val jobQueue = SingleThreadJobQueue(
                 messengers, terminators, partitionFunction,
-                Job.EU(f1.keySet().first(), f1.values().first()).javaClass  //TODO: There must be a better way to do this
+                Job.EU::class.java as Class<Job.EU<N, C>>  //TODO: There must be a better way to do this
         )
 
-        fun pushBack(node: N, colors: C) {
-            for ((predecessor, edgeColors) in node.predecessors()) {    //push to predecessors
-                val intersection = edgeColors intersect colors
-                if (intersection.isNotEmpty()) jobQueue.post(Job.EU(predecessor, intersection))
-            }
-        }
+        fun pushBack(node: N, colors: C) = pushBack(node, colors, jobQueue) { s, t, c -> Job.EU(t, c) }
 
         //prepare queue
         for ((node, colors) in f1) {
@@ -95,7 +87,8 @@ public class ModelChecker<N: Node, C: Colors<C>>(
 
         jobQueue.start { val (node, colors) = it
             val intersection = colors intersect f0.getOrDefault(node)
-            if (result.addOrUnion(node, intersection)) pushBack(node, intersection)
+            val modified = synchronized(result) { result.addOrUnion(node, intersection) }
+            if (modified) pushBack(node, intersection)
         }
 
         jobQueue.finish()
@@ -108,14 +101,50 @@ public class ModelChecker<N: Node, C: Colors<C>>(
         val f0 = verify(f[0])
         val f1 = verify(f[1])
         val result = HashMap<N, C>().withDefaultMutable(f0.default)
+        val uncoveredEdges = HashMap<N, MutableMap<N, C>>()
 
+        val jobQueue = SingleThreadJobQueue(
+                messengers, terminators, partitionFunction,
+                Job.AU::class.java as Class<Job.AU<N, C>>  //TODO: There must be a better way to do this
+        )
 
+        fun pushBack(node: N, colors: C) = pushBack(node, colors, jobQueue) { s, t, c -> Job.AU(s, t, c) }
+
+        for ((node, colors) in f1) pushBack(node, colors)
+
+        jobQueue.start { val (source, node, colors) = it
+            synchronized(uncoveredEdges) {
+                if (node !in uncoveredEdges) uncoveredEdges[node] = HashMap(node.successors())
+            }
+            val uncoveredSuccessors = uncoveredEdges[node]!!
+            val validColors = synchronized(uncoveredSuccessors) {
+                //Would this be reasonably faster if we removed empty sets from map completely?
+                uncoveredSuccessors[source] == uncoveredSuccessors[source]!! - colors
+
+                //Or should we cache results of this reduction?
+                f0.getOrDefault(node) intersect (colors - uncoveredSuccessors.values().reduce { a, b -> a union b })
+            }
+            if (validColors.isNotEmpty()) {
+                val modified = synchronized(result) { result.addOrUnion(node, validColors) }
+                if (modified) pushBack(node, validColors)
+            }
+        }
+
+        jobQueue.finish()
         return result.toMap()
+    }
+
+    private fun <J: Job<N, C>> pushBack(node: N, colors: C, jobQueue: JobQueue<N, C, J>, jobConstructor: (N, N, C) -> J) {
+        for ((predecessor, edgeColors) in node.predecessors()) {
+            val intersection = edgeColors intersect colors
+            if (intersection.isNotEmpty()) jobQueue.post(jobConstructor(node, predecessor, intersection))
+        }
     }
 
     private fun <T> globalBarrier(f: () -> T): T {
         return GlobalBarrier(terminators.createNew(), f)()
     }
+
 }
 
 class GlobalBarrier<T>(

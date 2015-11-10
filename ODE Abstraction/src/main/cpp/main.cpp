5a6ffa6956230c6d16ff1830e3c8a67ee57c10bc
martindemko <325073@mail.muni.cz>
2015-11-10 22:40:28 +0100

    testing main again

--- ODE Abstraction/src/main/cpp/main.cpp
+++ ODE Abstraction/src/main/cpp/main.cpp
@@ -23,24 +23,43 @@ int main(int argc, char** argv) {
         model.RunAbstraction();
 
 		StateSpaceGenerator * generator = new StateSpaceGenerator(model, true);
+	    std::string var = model.getVariable(0);
+	    /*
+	    std::vector<std::list<std::pair<double, double> > > paramSpace;
+        for(int i = 0; i < model.getParamSize(); i++) {
+            std::list<std::pair<double, double> > param;
+            param.push_back(std::pair<double, double>(model.getParamRanges().at(i)));
+            paramSpace.push_back(param);
+        }*/
+        vector<pair<double,double> > paramSpace = model.getParamRanges();
+		vector<State> inits = generator->initAP(var,Operators::LS,5,paramSpace);
 
-        std::cout << model.getParamRanges()[0].first << " " << model.getParamRanges()[0].second << std::endl;
+        for(State s : inits) {
+            vector<State> data = generator->getSucc(s);
+            std::cout << "Succ for: " << s << std::endl;
+            for (int j = 0; j < data.size(); ++j){
+                std::cout << data.at(j) << std::endl;
+            }
+            std::cout << std::endl;
+        }
+/*
+        std::cout << model.getParamRanges().at(0).first << " " << model.getParamRanges().at(0).second << std::endl;
 
 		std::vector<std::list<std::pair<double, double> > > paramSpace;
 		std::list<std::pair<double, double> > param;
-		param.push_back(std::pair<double, double>(model.getParamRanges()[0]));
+		param.push_back(std::pair<double, double>(model.getParamRanges().at(0)));
 		paramSpace.push_back(param);
 
         for (long unsigned int i=0; i<model.getThresholdsForVariable(0).size()-1; i++) {
-        	State s(1, {i}, paramSpace);
+        	State s(1, {i,0}, paramSpace);
         	vector<State> data = generator->getSucc(s);	
-        	std::cout << "Succ for: " << model.getThresholdForVarByIndex(0, s.getCoors()[0]) << " " << s;
-        	for (int i = 0; i < data.size(); ++i)
+        	std::cout << "Succ for: " << model.getThresholdForVarByIndex(0, s.getCoors().at(0)) << " " << s;
+        	for (int j = 0; j < data.size(); ++j)
         	{
-        		std::cout << data[i];
+        		std::cout << data.at(j);
         	}
         }
-
+*/
         
        
     }
